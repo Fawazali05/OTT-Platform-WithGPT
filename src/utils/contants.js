4359feeaaef08ed591d4752166f494ff9105df1b
Fawazali05 <fawazali.fas@gmail.com>
2024-09-24 00:53:16 +0530

    token

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -2,6 +2,6 @@ export const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYWVjYzdmODZmOWRlOTJmMTZlY2I3MGMwNTgxYTUwOSIsIm5iZiI6MTcyNjY3OTY2Mi4yNDM0NzQsInN1YiI6IjY2ZTljZTE0NWMwNTE5YTIzNGQzMDQ1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l_2hjlkt09S6hiV6fY-_XqCzDAO7bcgbz23Gaz3vQpY'
+      Authorization: 'Token'
     }
 }
